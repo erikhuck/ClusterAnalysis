5994ca06922bdba379353d40e6d3ea87b22d4bf2
Erik Huckvale <erikhuck@gmail.com>
2020-02-24 17:47:11 -0700

    Now incorporating targets into the data set for supervised learning

--- .job.sh
+++ .job.sh
@@ -5,8 +5,8 @@ JOB_NAME="data_clean"
 sbatch -J $JOB_NAME \
     --time=00-05:00:00 \
     --nodes=1 \
-    --ntasks=4 \
-    --mem=64G \
+    --ntasks=5 \
+    --mem=128G \
     -o .${JOB_NAME}.out \
     -e .${JOB_NAME}.err \
     .${JOB_NAME}.sh
